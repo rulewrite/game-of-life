3fac25340fc61a55f3c40408522a4231a5d17ea5
rulewrite <rulewrite@gmail.com>
2022-10-11 23:04:58 +0900

    feat: 텍스트로 변환 Display 트레잇 구현

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::fmt;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -100,5 +102,19 @@ impl Universe {
         }
 
         self.cells = next;
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
     }
 }
